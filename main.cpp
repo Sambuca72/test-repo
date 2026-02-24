5be352ccff32b664d0d002b0ea6634c851d7ec56
cyberpsychoz <nik631stal@mail.ru>
2026-02-24 17:42:08 +0700

    test 2.2

--- main.cpp
+++ main.cpp
@@ -70,3 +70,46 @@ EXPECT_FALSE(dojCandle.body_contains(10.1));
 }
 
   
+// Test contains()
+
+TEST_F(CandleTest, Contains_GreenCandle) {
+
+EXPECT_TRUE(greenCandle.contains(8.0));
+
+EXPECT_TRUE(greenCandle.contains(10.0));
+
+EXPECT_TRUE(greenCandle.contains(15.0));
+
+EXPECT_TRUE(greenCandle.contains(12.0));
+
+EXPECT_FALSE(greenCandle.contains(7.0));
+
+EXPECT_FALSE(greenCandle.contains(16.0));
+
+}
+
+  
+
+TEST_F(CandleTest, Contains_Boundaries) {
+
+Candle candle{5.0, 10.0, 0.0, 7.0};
+
+EXPECT_TRUE(candle.contains(0.0)); 
+
+EXPECT_TRUE(candle.contains(10.0)); 
+
+EXPECT_FALSE(candle.contains(-0.1));
+
+EXPECT_FALSE(candle.contains(10.1));
+
+}
+
+  
+
+TEST_F(CandleTest, Contains_OutOfRange) {
+
+EXPECT_FALSE(greenCandle.contains(7.5));
+
+EXPECT_FALSE(greenCandle.contains(15.5));
+
+}
