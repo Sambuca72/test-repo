769eecb9b8fbb634a0c8ca99c190ac78d5e9e46a
Sambuca72 <artemstolyarov91567@gamil.com>
2026-05-13 10:28:31 +0700

    Initialize project and update main.cpp

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "<html><body><pre>" > /usr/share/nginx/html/index.html
-
-docker logs app_container >> /usr/share/nginx/html/index.html
-
-echo "</pre></body></html>" >> /usr/share/nginx/html/index.html
-
-exec nginx -g "daemon off;"
